770cdb89dd48f942504b54df01c4c491ecdf6e90
Brad Waropay <bradwaropay@Brads-MacBook-Pro.local>
2015-08-28 09:35:16 -0700

    New breakout and styling

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -6,10 +6,16 @@
       width: document.getElementsByClassName('splash-header')[0].offsetWidth,
       cell_size: 100,
       x_colors: ['#7a1631', '#3f0b1b', '#7a1631'],
-      y_colors: ['#fc7d49', '#3f0b1b', '#fc7d49'],
+      y_colors: ['#fc7d49', '#3f0b1b', '#7a1631'],
     });
 
-    document.getElementsByClassName('splash-header')[0].appendChild(featuredWorkBackground.canvas());
+    $('.splash-header').each(function() {
+      $(this).append(featuredWorkBackground.canvas());
+    });
+  }
+
+  function clearFeaturedWorkBackground(){
+    $('.splash-header canvas').remove();
   }
 
   setFeaturedWorkBackground();
@@ -19,6 +25,7 @@
   $(window).on('resize', function(e) {
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(function() {
+      clearFeaturedWorkBackground();
       setFeaturedWorkBackground();
     }, 250);
   });
