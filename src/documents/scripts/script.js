d5560bdceb93e3cc169ae89eae7bf219bfa14e95
Brad Waropay <Brad.waropay@gmail.com>
2015-09-22 00:02:49 -0700

    Seperate content partials

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -56,21 +56,21 @@
   // Initialize and append trianglify
   function setFeaturedWorkBackground(){
     var featuredWorkBackground = Trianglify({
-      height: document.getElementsByClassName('splash-header')[0].offsetHeight,
-      width: document.getElementsByClassName('splash-header')[0].offsetWidth,
+      height: document.getElementsByClassName('content-section--trianglify')[0].offsetHeight,
+      width: document.getElementsByClassName('content-section--trianglify')[0].offsetWidth,
       cell_size: 100,
       x_colors: ['#7a1631', '#3f0b1b', '#7a1631'],
       y_colors: ['#fc7d49', '#3f0b1b', '#7a1631'],
     });
 
-    $('.splash-header').each(function() {
+    $('.content-section--trianglify').each(function() {
       $(this).append(featuredWorkBackground.canvas());
     });
   }
 
   // Remove old trianglify
   function clearFeaturedWorkBackground(){
-    $('.splash-header canvas').remove();
+    $('.content-section--trianglify canvas').remove();
   }
 
   setFeaturedWorkBackground();
