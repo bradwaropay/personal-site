674b7df3ff18663ba154fff8059af222fab95fe6
Brad Waropay <Brad.waropay@gmail.com>
2016-03-19 17:04:02 -0700

    Comment background-particles

--- src/scripts/background-particles.js
+++ src/scripts/background-particles.js
@@ -1,18 +1,22 @@
 var backgroundParticles = function() {
+  // Get canvas, set context, and particle colors
   var canvas = document.getElementById('background-particles'),
       ctx = canvas.getContext('2d'),
       colorParticle = '#422727',
       colorConnect = '#422727';
 
+  // Create particles array and set reaction distance
   var particles = {
-    distance: 60,
-    array: []
+    array: [],
+    distance: 60
   };
 
+  // Set reaction radius on mouse position
   var mousePosition = {
     radius: 100,
   };
 
+  // Construct particle
   function Particle() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
@@ -23,6 +27,7 @@ var backgroundParticles = function() {
     this.radius = Math.random();
   }
 
+  // Create, animate, and connect particle
   Particle.prototype = {
     create: function() {
       ctx.beginPath();
@@ -69,6 +74,7 @@ var backgroundParticles = function() {
     }
   };
 
+  // Create particles
   function createParticles() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -82,17 +88,20 @@ var backgroundParticles = function() {
     particle.animate();
   }
 
+  // Get mouse position
   window.onmousemove = function(position) {
     mousePosition.x = position.pageX;
     mousePosition.y = position.pageY - window.scrollY;;
   }
 
+  // Draw canvas
   function redrawCanvas() {
     ctx.fillStyle = colorParticle;
     ctx.lineWidth = .1;
     ctx.strokeStyle = colorConnect;
   }
 
+  // Adjust particle count according to screen area
   function scaleParticleCount() {
     particles.nb = Math.round((canvas.width * canvas.height) * .0004);
 
@@ -101,6 +110,8 @@ var backgroundParticles = function() {
     particles.array.length = particles.nb;
   }
 
+  // Resize and redraw canvas
+  // Adjust particles according to new area and new boundries
   function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -123,9 +134,12 @@ var backgroundParticles = function() {
     }
   }
 
+  // Create and animate particles
   setInterval(createParticles, 1000/30);
 
+  // Listen for resize
   window.addEventListener('resize', resizeCanvas, false)
 
+  // Draw canvas and create initial particles
   resizeCanvas();
 };
