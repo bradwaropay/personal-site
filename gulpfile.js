2e4429c9571e7b7379fcda3d5fa2350772c160eb
Brad Waropay <bradwaropay@Brads-MacBook-Pro.local>
2017-06-22 00:10:08 -0700

    Add function options and rename to speckle

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Include gulp
-var gulp = require('gulp');
+var gulp = require('gulp')
 
 // Include plugins
 var plugins = require('gulp-load-plugins')({
@@ -11,8 +11,8 @@ var plugins = require('gulp-load-plugins')({
     'main-bower-files',
     'precss'
   ],
-  replaceString: /\b(gulp|postcss)[\-.]/
-});
+  replaceString: /\b(gulp|postcss)[-.]/
+})
 
 // CSS and JS paths
 var paths = {
@@ -25,20 +25,20 @@ var paths = {
 }
 
 // Get Bower dependencies and compile lib CSS
-gulp.task('bower-css', function() {
-   gulp.src(plugins.mainBowerFiles())
+gulp.task('bower-css', function () {
+  gulp.src(plugins.mainBowerFiles())
     .pipe(plugins.filter('*.css'))
     .pipe(plugins.order([
       '*'
     ]))
     .pipe(plugins.concat('lib.css'))
     .pipe(plugins.minifyCss({keepSpecialComments: 0}))
-    .pipe(gulp.dest(paths.cssDestination));
-});
+    .pipe(gulp.dest(paths.cssDestination))
+})
 
 // Get Bower dependencies and compile lib JS
-gulp.task('bower-js', function() {
-   gulp.src(plugins.mainBowerFiles())
+gulp.task('bower-js', function () {
+  gulp.src(plugins.mainBowerFiles())
     .pipe(plugins.filter('*.js'))
     .pipe(plugins.order([
       'jquery.js',
@@ -46,59 +46,58 @@ gulp.task('bower-js', function() {
     ]))
     .pipe(plugins.concat('lib.js'))
     .pipe(plugins.uglify())
-    .pipe(gulp.dest(paths.jsDestination));
-});
+    .pipe(gulp.dest(paths.jsDestination))
+})
 
 // Include PostCSS processors
 var processors = [
   plugins.precss(),
   plugins.hexrgba(),
   plugins.autoprefixer()
-];
+]
 
 // Compile CSS
-gulp.task('compile-css', function() {
+gulp.task('compile-css', function () {
   return gulp.src(paths.cssSource + '**/*.css')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.postcss(processors))
     .pipe(plugins.filter('style.css'))
     .pipe(plugins.minifyCss())
     .pipe(plugins.sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.cssDestination));
-});
+    .pipe(gulp.dest(paths.cssDestination))
+})
 
 // Clean CSS
 gulp.task('clean-css', ['compile-css'], function () {
   return gulp.src(paths.out + '*.css', {read: false})
     .pipe(plugins.clean())
-});
+})
 
 // Compile JS
-gulp.task('compile-js', function() {
+gulp.task('compile-js', function () {
   return gulp.src(paths.jsSource + '**/*.js')
     .pipe(plugins.order([
       'webfonts.js',
       'background-particles.js',
-      'logo-loader.js',
       'smoothstate.js',
       'script.js'
     ]))
     .pipe(plugins.concat('script.js'))
     // .pipe(plugins.uglify())
-    .pipe(gulp.dest(paths.jsDestination));
-});
+    .pipe(gulp.dest(paths.jsDestination))
+})
 
 // Clean JS
 gulp.task('clean-js', ['compile-js'], function () {
   return gulp.src(paths.out + '*.js', {read: false})
     .pipe(plugins.clean())
-});
+})
 
 // Clean Data
 gulp.task('clean-data', function () {
   return gulp.src(paths.out + 'data', {read: false})
     .pipe(plugins.clean())
-});
+})
 
 // Tasks
-gulp.task('default', ['bower-css', 'bower-js', 'clean-css', 'clean-data']);
+gulp.task('default', ['bower-css', 'bower-js', 'clean-css', 'clean-data'])
