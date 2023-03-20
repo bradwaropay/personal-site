2844eeffd83d7641175ecc7ca5f9ff187d1292a1
bradwaropay <brad.waropay@gmail.com>
2023-03-20 08:13:55 -0500

    feat: :tada: Initialize Nuxt 3

--- plugins/vueScrollBehavior.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import vueScrollBehavior from 'vue-scroll-behavior'
-
-export default function ({ app }) {
-  Vue.use(vueScrollBehavior, { router: app.router, el: '.app' })
-}
