f42cdde1d4c693ea11327ffe15237e599f33ebdf
bradwaropay <brad.waropay@gmail.com>
2023-04-28 19:54:47 -0500

    Continue building

--- assets/scripts/tokens.ts
+++ assets/scripts/tokens.ts
@@ -1,26 +1,33 @@
-import Colors from '~/assets/styles/exports/colors.module.scss';
+import ColorsSource from '~/assets/styles/exports/colors.module.scss';
 
 import { setDeepObject } from './utils';
 
-export const Color = {
+export const Colors = {
   palette: {},
   theme: {}
 };
 
-for (const [key, value] of Object.entries(Colors)) {
+for (const [key, value] of Object.entries(ColorsSource)) {
   const type = key.split('-')[1];
 
   switch (type) {
     case 'palette':
       const paletteKeys = key.split('color-palette-').pop()?.split('-') || [];
       // createNestedObject(Palette, paletteKeys, value);
-      setDeepObject(Color.palette, paletteKeys, value);
+      setDeepObject(Colors.palette, paletteKeys, value);
       break;
     case 'theme':
       const themeKeys = key.split('color-theme-').pop()?.split('-') || [];
-      setDeepObject(Color.theme, themeKeys, value);
+      setDeepObject(Colors.theme, themeKeys, value);
     break;
     default:
       break;
   }
 }
+
+export const Typpgraphy = {};
+
+export const Motion = {};
+
+export const Layout = {};
+
